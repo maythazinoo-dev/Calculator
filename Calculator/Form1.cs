cdcbaed2d1b4e7028c5f66413d6e27dbceb48cff
May Thazin Oo <maythazinoo18817@gmail.com>
2025-03-17 05:56:17 +0630

    fix: Calculator

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -221,110 +221,110 @@ namespace Calculator
             int lastOperator = textBox1.Text.LastIndexOfAny(AriOperator);
             firstNum = double.Parse(textBox1.Text.Substring(0, firstOperator));
             secondNum = double.Parse(textBox1.Text.Substring(lastOperator + 1));
-            //operation = textBox1.Text.Substring(firstOperator, 1);// work first operator
+            operation = textBox1.Text.Substring(firstOperator, 1);// work first operator
             //operation = textBox1.Text.Substring(lastOperator,1);// work last operator
 
-            //switch (operation)
-            //{
-            //    case "+":
-            //        result = firstNum + secondNum;
-            //        //result += secondNum;
-            //        break;
-            //    case "-":
-            //        result = firstNum - secondNum;
-            //        break;
-            //    case "*":
-            //        result = firstNum * secondNum;
-            //        break;
-            //    case "/":
-            //        result = (secondNum != 0) ? firstNum / secondNum : double.NaN;
-            //        break;
-            //    case "^":
-            //        result = Math.Pow(firstNum, secondNum);
-            //        break;
-            //}
-            //textBox1.Text = result.ToString();
-
-            string input = textBox1.Text;
-            string currentNumber ="";
-            List<double> numbers = new List<double>();
-            List<char> operators = new List<char>();
-           
-
-            foreach(char ch in input)
+            switch (operation)
             {
-                if (char.IsDigit(ch) || ch == '.')
-                {
-
-                    currentNumber += ch;
-                }
-                else if (AriOperator.Contains(ch))
-                {
-                    if (!string.IsNullOrEmpty(currentNumber))
-                    {
-                        numbers.Add(double.Parse(currentNumber));
-                        currentNumber = "";
-                    }
-                    operators.Add(ch);
-                }
-            }
-
-
-
-            if(currentNumber != "")
-            {
-                numbers.Add(double.Parse(currentNumber));
-            }
-
-            if (numbers.Count == 0 || numbers.Count != operators.Count + 1)
-            {
-                textBox1.Text = "Error";
-                return;
-            }
-
-            for (int i = 0; i < operators.Count; i++)
-            {
-                if (operators[i] == '^')
-                {
-                    numbers[i] = Math.Pow(numbers[i], numbers[i + 1]);
-                    numbers.RemoveAt(i + 1);
-                    operators.RemoveAt(i);
-                    i--;
-                }
-            }
-
-            for (int i = 0; i < operators.Count; i++)
-            {
-                if (operators[i] == '*' || operators[i] == '/')
-                {
-                    if (operators[i] == '*')
-                        numbers[i] *= numbers[i + 1];
-                    else if (numbers[i + 1] != 0)
-                        numbers[i] /= numbers[i + 1];
-                    else
-                    {
-                        textBox1.Text = "Error (Divide by 0)";
-                        return;
-                    }
-                    numbers.RemoveAt(i + 1);
-                    operators.RemoveAt(i);
-                    i--;
-                }
-            }
-
-            double result = numbers[0];
-            for (int i = 0; i < operators.Count; i++)
-            {
-                if (operators[i] == '+')
-                    result += numbers[i + 1];
-                else if (operators[i] == '-')
-                    result -= numbers[i + 1];
+                case "+":
+                    result = firstNum + secondNum;
+                    //result += secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+                case "/":
+                    result = (secondNum != 0) ? firstNum / secondNum : double.NaN;
+                    break;
+                case "^":
+                    result = Math.Pow(firstNum, secondNum);
+                    break;
             }
             textBox1.Text = result.ToString();
+
+            //    string input = textBox1.Text;
+            //    string currentNumber ="";
+            //    List<double> numbers = new List<double>();
+            //    List<char> operators = new List<char>();
+
+
+            //    foreach(char ch in input)
+            //    {
+            //        if (char.IsDigit(ch) || ch == '.')
+            //        {
+
+            //            currentNumber += ch;
+            //        }
+            //        else if (AriOperator.Contains(ch))
+            //        {
+            //            if (!string.IsNullOrEmpty(currentNumber))
+            //            {
+            //                numbers.Add(double.Parse(currentNumber));
+            //                currentNumber = "";
+            //            }
+            //            operators.Add(ch);
+            //        }
+            //    }
+
+
+
+            //    if(currentNumber != "")
+            //    {
+            //        numbers.Add(double.Parse(currentNumber));
+            //    }
+
+            //    if (numbers.Count == 0 || numbers.Count != operators.Count + 1)
+            //    {
+            //        textBox1.Text = "Error";
+            //        return;
+            //    }
+
+            //    for (int i = 0; i < operators.Count; i++)
+            //    {
+            //        if (operators[i] == '^')
+            //        {
+            //            numbers[i] = Math.Pow(numbers[i], numbers[i + 1]);
+            //            numbers.RemoveAt(i + 1);
+            //            operators.RemoveAt(i);
+            //            i--;
+            //        }
+            //    }
+
+            //    for (int i = 0; i < operators.Count; i++)
+            //    {
+            //        if (operators[i] == '*' || operators[i] == '/')
+            //        {
+            //            if (operators[i] == '*')
+            //                numbers[i] *= numbers[i + 1];
+            //            else if (numbers[i + 1] != 0)
+            //                numbers[i] /= numbers[i + 1];
+            //            else
+            //            {
+            //                textBox1.Text = "Error (Divide by 0)";
+            //                return;
+            //            }
+            //            numbers.RemoveAt(i + 1);
+            //            operators.RemoveAt(i);
+            //            i--;
+            //        }
+            //    }
+
+            //    double result = numbers[0];
+            //    for (int i = 0; i < operators.Count; i++)
+            //    {
+            //        if (operators[i] == '+')
+            //            result += numbers[i + 1];
+            //        else if (operators[i] == '-')
+            //            result -= numbers[i + 1];
+            //    }
+            //    textBox1.Text = result.ToString();
+            //}
+
+
         }
-
-
-
 
     }
 }
